8ed234874f7d6f4dcdf6a1d78d10d5adc7a1b54a
RowanMaBoot <nobody@rccclan.com>
2025-06-20 11:24:29 -0500

    Add settings

---
+++ ActorRefFixes/SettingsGeneral.cs
@@ -0,0 +1,42 @@
+using Mutagen.Bethesda.Synthesis.Settings;
+
+namespace ActorRefFixes
+{
+    public class SettingsGeneral
+    {
+        [SynthesisOrder]
+        [SynthesisSettingName("Enable Cell XLCN Updates")]
+        [SynthesisTooltip("This setting attempts to update the Location (XLCN) of cells, using their parent worldspace as a point of reference.\n\nDisabling this option may impact population of some NPC XLCN records, as some NPCs are associated with Cells that have no location.\n\nHowever, it should reduce the number of patched records significantly and therefore the number of masters in use.")]
+        public bool enableCellLocationUpdates;
+
+        [SynthesisOrder]
+        [SynthesisSettingName("Enable NPC XLCN Updates")]
+        [SynthesisTooltip("This setting attempts to update the Exit Location (XLCN) of Actor References based on the Persistent Location of their parent cell.\n\nIf the cell has no persistent location keyword associated with it, the ref will be skipped.")]
+        public bool enableNPCPersistentLocationUpdates;
+
+        public ActorUpdates actorUpdateSettings = new();
+    }
+
+    public class ActorUpdates
+    {
+        [SynthesisOrder]
+        [SynthesisSettingName("Update Animal Refs")]
+        public bool updateAnimals;
+
+        [SynthesisOrder]
+        [SynthesisSettingName("Update Creature Refs")]
+        public bool updateCreatures;
+
+        [SynthesisOrder]
+        [SynthesisSettingName("Update Dwemer Refs")]
+        public bool updateDwemer;
+
+        [SynthesisOrder]
+        [SynthesisSettingName("Update Humanoid Refs")]
+        public bool updateHumanoids;
+
+        [SynthesisOrder]
+        [SynthesisSettingName("Update Undead Refs")]
+        public bool updateUndead;
+    }
+}
