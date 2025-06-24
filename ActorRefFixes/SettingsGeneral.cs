55f3fddd76c31de6ab9971ce75d9b259116cadf4
RowanMaBoot <nobody@rccclan.com>
2025-06-24 08:47:20 -0500

    Update SettingsGeneral.cs

--- ActorRefFixes/SettingsGeneral.cs
+++ ActorRefFixes/SettingsGeneral.cs
@@ -7,12 +7,12 @@ namespace ActorRefFixes
         [SynthesisOrder]
         [SynthesisSettingName("Enable Cell XLCN Updates")]
         [SynthesisTooltip("This setting attempts to update the Location (XLCN) of cells, using their parent worldspace as a point of reference.\n\nDisabling this option may impact population of some NPC XLCN records, as some NPCs are associated with Cells that have no location.\n\nHowever, it should reduce the number of patched records significantly and therefore the number of masters in use.")]
-        public bool enableCellLocationUpdates;
+        public bool enableCellLocationUpdates = true;
 
         [SynthesisOrder]
         [SynthesisSettingName("Enable NPC XLCN Updates")]
         [SynthesisTooltip("This setting attempts to update the Exit Location (XLCN) of Actor References based on the Persistent Location of their parent cell.\n\nIf the cell has no persistent location keyword associated with it, the ref will be skipped.")]
-        public bool enableNPCPersistentLocationUpdates;
+        public bool enableNPCPersistentLocationUpdates = true;
 
         public ActorUpdates actorUpdateSettings = new();
     }
@@ -21,22 +21,22 @@ namespace ActorRefFixes
     {
         [SynthesisOrder]
         [SynthesisSettingName("Update Animal Refs")]
-        public bool updateAnimals;
+        public bool updateAnimals = true;
 
         [SynthesisOrder]
         [SynthesisSettingName("Update Creature Refs")]
-        public bool updateCreatures;
+        public bool updateCreatures = true;
 
         [SynthesisOrder]
         [SynthesisSettingName("Update Dwemer Refs")]
-        public bool updateDwemer;
+        public bool updateDwemer = true;
 
         [SynthesisOrder]
         [SynthesisSettingName("Update Humanoid Refs")]
-        public bool updateHumanoids;
+        public bool updateHumanoids = true;
 
         [SynthesisOrder]
         [SynthesisSettingName("Update Undead Refs")]
-        public bool updateUndead;
+        public bool updateUndead = true;
     }
 }
